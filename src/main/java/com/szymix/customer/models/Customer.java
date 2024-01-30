f840e787e37174d00ade1c90b8029ab65cdad393
Koziol, Szymon <Szymon.Koziol@PROSTEP.com>
2024-01-30 17:22:09 +0100

    adding flyway dependencies + basic setup

--- src/main/java/com/szymix/customer/models/Customer.java
+++ src/main/java/com/szymix/customer/models/Customer.java
@@ -3,18 +3,29 @@ package com.szymix.customer.models;
 import jakarta.persistence.*;
 
 @Entity
+@Table(
+        name = "customer",
+        uniqueConstraints = {
+            @UniqueConstraint(
+                    name = "customer_email_unique",
+                    columnNames = "email"
+            )
+        }
+)
 public class Customer {
 
     @Id
     @SequenceGenerator(
             name = "customer_id_seq",
-            sequenceName = "customer_id_seq"
+            sequenceName = "customer_id_seq",
+            allocationSize = 1
     )
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_id_seq")
     private Long id;
 
     @Column(nullable = false)
     private String name;
+    @Column(nullable = false)
     private String email;
     private Integer age;
 
